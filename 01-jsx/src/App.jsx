ff39154b4ba5dc30e59644f05c85f73df0c780a5
dipeshrathod07 <rathoddipesh221@gmail.com>
2026-05-16 17:24:10 +0530

    feat(react): add JSX learning module

---
+++ 01-jsx/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>My Profile</h1>
+        <p>Name : Dipesh Rathod</p>
+        <p>Role : React Learner</p>
+        <p>Goal : I Want to become a React developer</p>
+      </div>
+    </>
+  )
+}
+
+export default App
